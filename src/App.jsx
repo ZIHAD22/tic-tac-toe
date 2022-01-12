c60ee55306a9b2959b7bd903e1e91b213930ddaf
ZIHAD22 <mdzihad2244@gmail.com>
2022-01-12 19:19:12 +0600

    publish

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Game from "./components/game";
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <Game />
+    </div>
+  );
+};
+
+export default App;
